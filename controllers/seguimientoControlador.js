55d65c850846cec501ce78441532fbaaff11e6c9
Juan Saa A <juan96saa@gmail.com>
2023-04-10 22:07:06 -0500

    correcion en carga num comprobante

--- controllers/seguimientoControlador.js
+++ controllers/seguimientoControlador.js
@@ -1,12 +1,16 @@
-// import models from '../models';
-// import Producto from '../models/productos';
+import Venta from '../models/ventas';
 // import Inventario from '../models/inventario';
 
 export default {
     historial: async(req, res, next)=>{
       try {
-        res.status(200).send({message:"Respondiendo desde el server ok"})
+        const ventas = await Venta.find({
+          "detalles.producto": 'COCA COLA ORIGINAL 300ML', 
+          "detalles.codigoInventario": '5ff8b9182971240017f3c4b0'
+        })
+        res.send({message: ventas })
       } catch (error) {
+        console.log( error );
           res.status(500).send({
               message:error
           });
